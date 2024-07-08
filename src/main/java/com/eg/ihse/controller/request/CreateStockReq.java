0d814e8ec7418a7b9a4dd7748ade04cfb65256d5
ermang <ermangonul17@gmail.com>
2024-07-08 13:32:55 +0300

    add: request validation generic error response

--- src/main/java/com/eg/ihse/controller/request/CreateStockReq.java
+++ src/main/java/com/eg/ihse/controller/request/CreateStockReq.java
@@ -1,6 +1,10 @@
 package com.eg.ihse.controller.request;
 
+import javax.validation.constraints.NotBlank;
+
 public class CreateStockReq {
+    @NotBlank(message = "name can not be blank")
     public String name;
+    @NotBlank(message = "description can not be blank")
     public String description;
 }
---
+++ src/main/java/com/eg/ihse/util/Constant.java
@@ -0,0 +1,6 @@
+package com.eg.ihse.util;
+
+public class Constant {
+    public static final String OOPS_SOMETHING_UNEXPECTED_HAPPENED = "Something unexpected happened, sorry :(";
+
+}
